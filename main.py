e426725269b14829df1f7628e041c0d7d7f2756e
Vinicius Giuliani <vinicius.giuliani@gmail.com>
2024-10-18 16:22:56 -0300

    Adds frame rate cap of 60 FPS

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    # Internal clock and delta time
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +21,9 @@ def main():
         color = 'black'
         screen.fill(color)
         pygame.display.flip()
+
+        # Sets the frame rate to 60 and tracks the elapsed time for each frame
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
