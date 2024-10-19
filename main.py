388b24bbea485373da5767582c623440cf959aaa
Vinicius Giuliani <vinicius.giuliani@gmail.com>
2024-10-19 19:57:32 -0300

    Draws the player sprite onto the screen

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen width: {SCREEN_HEIGHT}")
-
+    # Starts the game and sets display mode
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -13,13 +12,23 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    # Instantiate player object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
+        # Quits the game when pressing the window's close button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # Fills the screen with the color black
         color = 'black'
         screen.fill(color)
+
+        # Draws player on screen
+        player.draw(screen)
+        
+        # Updates the display surface to the screen
         pygame.display.flip()
 
         # Sets the frame rate to 60 and tracks the elapsed time for each frame
---
+++ player.py
@@ -0,0 +1,23 @@
+import pygame
+
+from constants import *
+from circleshape import *
+
+# Base class for the player object
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        color = "white"
+        line_width = 2
+        pygame.draw.polygon(screen, color, self.triangle(), 2)
