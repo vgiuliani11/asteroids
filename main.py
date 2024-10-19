24742cab69d3857e6cf74971c0bda29b469d4f40
Vinicius Giuliani <vinicius.giuliani@gmail.com>
2024-10-19 20:26:04 -0300

    Moves the player forward, backward, and rotates left and right

--- main.py
+++ main.py
@@ -20,19 +20,22 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         # Fills the screen with the color black
         color = 'black'
         screen.fill(color)
 
         # Draws player on screen
         player.draw(screen)
+
+        # Updates the player position
+        player.update(dt)
         
         # Updates the display surface to the screen
         pygame.display.flip()
 
         # Sets the frame rate to 60 and tracks the elapsed time for each frame
         dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
