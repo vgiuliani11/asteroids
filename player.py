28d33d021f6b6539fa8bfce3ec1c364c4cb204d2
Vinicius Giuliani <vinicius.giuliani@gmail.com>
2024-10-21 17:34:16 -0300

    Sets shoot cooldown to 0.3 seconds

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_cooldown = 0
 
     def draw(self, screen):
         color = "white"
@@ -27,6 +28,9 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= dt
+            
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -49,6 +53,9 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.shoot_cooldown > 0:
+            return
+
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-
+        self.shoot_cooldown = PLAYER_SHOOT_COOLDOWN
